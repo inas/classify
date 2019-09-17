b5401f4abebcfd3f4a866ff9d63da0073d8b0d0b
anishainas <t-anisha.izdihar@traveloka.com>
2019-09-17 08:30:27 +0700

    add nav host fragment to main activity layout

--- app/src/main/java/inas/anisha/classify/MainActivity.kt
+++ app/src/main/java/inas/anisha/classify/MainActivity.kt
@@ -2,15 +2,11 @@ package inas.anisha.classify
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val host = NavHostFragment.create(R.navigation.navigation_graph)
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, host)
-            .setPrimaryNavigationFragment(host).commit()
     }
 }
